09577f13ace6cbe4ebdfd8a000f78995176cc741
Jigar3 <chavada.jigar.3@gmail.com>
2018-06-23 20:46:34 +0530

    heroku

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-const publicPath = path.join(__dirname, '..', "public");
+const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
