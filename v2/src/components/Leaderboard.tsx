3799949f4137dccde4a1abd62c108f8c719c9eeb
Jigar3 <chavada.jigar.3@gmail.com>
2019-01-10 01:22:59 +0530

    styled the leaderboard component

--- v2/src/components/Leaderboard.tsx
+++ v2/src/components/Leaderboard.tsx
@@ -16,17 +16,40 @@ class Leaderboard extends React.Component {
     
     render() {
         return (
-            <React.Fragment>
-                { !this.state.gotData ? "Loading....." : 
-                    this.state.leaderBoard.map((user, index) => (
-                        <tr key={user.creator}>
-                            <td> {index+1} </td>
-                            <td> {user.creator_name} </td>
-                            <td> {user.profitLoss} </td>
-                        </tr>
-                    ))
-                }
-            </React.Fragment>
+            <section className="container leaderboard">
+                <table className="table is-hoverable is-fullwidth">
+                    <thead>
+						<tr>
+							<th>Sr. No</th>
+							<th>Name of the User</th>
+							<th>Total Profit</th>
+							<th>Total Stocks</th>
+							<th>Good Stocks</th>
+							<th>Bad Stocks</th>
+						</tr>
+					</thead>
+                    { !this.state.gotData ? <h1>Loading....</h1> : 
+                        <tbody>
+                            {
+                                this.state.leaderBoard.map((user, index) => {
+                                    if(index < 10) {
+                                        return (
+                                            <tr key={user.creator}>
+                                                <td> {index+1} </td>
+                                                <td> {user.creator_name} </td>
+                                                <td> $ {user.profitLoss} </td>
+                                                <td> {user.no_of_stocks} </td>
+                                                <td> {user.no_of_stocks_giving_profit} </td>
+                                                <td> {user.no_of_stocks_giving_loss} </td>
+                                            </tr>
+                                        )
+                                    }
+                                })
+                            }
+                        </tbody>
+                    }
+                </table>
+            </section>
         )
     }
 }
